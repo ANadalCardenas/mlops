1916f8a5eecc49319cdffcc86111c34e8e83060d
Aina Nadal <aina.nadal0@gmail.com>
2025-11-20 12:49:47 +0100

    Addapts the model to images (convert images to tensors)

--- session-2/main.py
+++ session-2/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -38,10 +39,12 @@ def eval_single_epoch(my_model, epoch,  dataloader, criterion, num_epochs):
 
 def train_model(config):
     
-    my_dataset = MyDataset(config["images_path"], config["labels_path"])
+    # Convert an image to a tensor
+    transform = transforms.Compose([transforms.Grayscale(),transforms.ToTensor()])
+    my_dataset = MyDataset(config["images_path"], config["labels_path"], transform=transform)
     dataloader = DataLoader(my_dataset, batch_size=config['batch_size'])
     my_model = MyModel(config['features'], config['hidden_layers'], config['outputs']).to(device)
-    criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(my_model.parameters(), config["lr"])
     for epoch in range(config["epochs"]):
         train_single_epoch(my_model, epoch,  dataloader, criterion, optimizer, config['epochs'])
@@ -57,7 +60,7 @@ if __name__ == "__main__":
 
     config = {
         "epochs": 5,
-        "batch_size": 10,
+        "batch_size": 15,
         "images_path": "/home/anadal/workspace/aidl-2025-winter-mlops/session-2/archive/data/data/",
         "labels_path": "/home/anadal/workspace/aidl-2025-winter-mlops/session-2/archive/chinese_mnist.csv",
         "features" : 4096,
