158fcc1e71bfc4dfc823fe04058ca19f68fe79cc
Aina Nadal <aina.nadal0@gmail.com>
2025-11-20 12:48:07 +0100

    Added torchvision module

--- session-2/main.py
+++ session-2/main.py
@@ -11,42 +11,45 @@ from torch.utils.data import DataLoader
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-"""
 
-def train_single_epoch(my_model, epoch,  dataloader, criterion, optimizer):
-    print(f"Epoch {epoch+1}/{config["epochs"]}")
+def train_single_epoch(my_model, epoch,  dataloader, criterion, optimizer, num_epochs):
+    print(f"Epoch {epoch+1}/{num_epochs}")
     for x, y in dataloader:
-        x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
+        x, y = x.to(device), y.to(device)        
         y_ = my_model(x)
         loss = criterion(y_, y)
         loss.backward()
         optimizer.step()
 
 
-def eval_single_epoch(my_model, epoch,  dataloader, criterion):
-    print(f"Epoch {epoch+1}/{config["epochs"]}")
+def eval_single_epoch(my_model, epoch,  dataloader, criterion, num_epochs):
+    print(f"Epoch {epoch+1}/{num_epochs}")
+    correct = 0
+    total = 0
     for x, y in dataloader:
         x, y = x.to(device), y.to(device)
         y_ = my_model(x)
-        loss = criterion(y_, y)
-        loss.backward()
+        preds = torch.argmax(y_, dim=1)
+        correct += (preds == y).sum().item()
+        total += y.size(0)
+    accuracy = correct / total
+    return  accuracy
 
-"""
 def train_model(config):
     
     my_dataset = MyDataset(config["images_path"], config["labels_path"])
     dataloader = DataLoader(my_dataset, batch_size=config['batch_size'])
     my_model = MyModel(config['features'], config['hidden_layers'], config['outputs']).to(device)
-    """
     criterion = nn.MSELoss()
-    optimizer = optim.SGD(my_model.parameters(), lr=args.lr)
+    optimizer = optim.SGD(my_model.parameters(), config["lr"])
     for epoch in range(config["epochs"]):
-        train_single_epoch(my_model, epoch,  dataloader, criterion, optimizer)
-        eval_single_epoch(my_model, epoch,  dataloader, criterion)
+        train_single_epoch(my_model, epoch,  dataloader, criterion, optimizer, config['epochs'])
+        with torch.no_grad():
+            eval_single_epoch(my_model, epoch,  dataloader, criterion, config['epochs'])
 
     return my_model
-    """
+    
     
 
 
@@ -59,6 +62,8 @@ if __name__ == "__main__":
         "labels_path": "/home/anadal/workspace/aidl-2025-winter-mlops/session-2/archive/chinese_mnist.csv",
         "features" : 4096,
         "hidden_layers": 64,
-        "outputs": 15
+        "outputs": 15,
+        "lr" : 0.1
+
     }
     train_model(config)
